3e75e225a62a023ac1d3d4e4286af534edefd46d
axel tavares <tavaresaxel95@gmail.com>
2024-05-27 17:01:28 -0400

    implemented validation for contact field

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,4 +1,56 @@
-function Contact(){
+import { useState } from 'react';
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+function Contact() {
+    const [email, setEmail] = useState('');
+    const [name, setName] = useState('');
+    const [message, setMessage] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const handleInputChange = (e) => {
+        // Getting the value and name of the input which triggered the change
+        const { target } = e;
+        const inputType = target.name;
+        const inputValue = target.value;
+
+        // Based on the input type, we set the state of either email, username, and password
+        if (inputType === 'email') {
+            setEmail(inputValue);
+        } else if (inputType === 'name') {
+            setName(inputValue);
+        } else {
+            setMessage(inputValue);
+        }
+    };
+
+    const handleFormSubmit = (e) => {
+        // Preventing the default behavior of the form submit (which is to refresh the page)
+        e.preventDefault();
+
+        // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
+        if (!validateEmail(email) || !name) {
+            setErrorMessage('Email or username is invalid');
+            // We want to exit out of this code block if something is wrong so that the user can correct it
+            return;
+            // Then we check to see if the password is not valid. If so, we set an error message regarding the password.
+        }
+
+        if(message == ""){
+            setErrorMessage("Please include a message");
+            return
+        }
+        alert(`Hello ${name}`);
+
+        // If everything goes according to plan, we want to clear out the input after a successful registration.
+        setName('');
+        setMessage('');
+        setEmail('');
+        setErrorMessage('');
+    };
+
     return <>
         <div class="container-xxl py-5" id="contact">
             <div class="container py-5 px-lg-5">
@@ -10,29 +62,27 @@ function Contact(){
                     <div class="col-lg-9">
                         <div class="wow fadeInUp" data-wow-delay="0.3s">
                             <p class="text-center mb-4">The contact form is currently inactive. Get a functional and working contact form with Ajax & PHP in a few minutes. Just copy and paste the files, add a little code and you're done. <a href="https://htmlcodex.com/contact-form">Download Now</a>.</p>
-                            <form>
+                            <form onSubmit={handleFormSubmit}>
                                 <div class="row g-3">
                                     <div class="col-md-6">
                                         <div class="form-floating">
-                                            <input type="text" class="form-control" id="name" placeholder="Your Name"/>
+                                            <input type="text" class="form-control" id="name" placeholder="Your Name" value={name}
+                                                name="name"
+                                                onChange={handleInputChange} />
                                             <label for="name">Your Name</label>
                                         </div>
                                     </div>
                                     <div class="col-md-6">
                                         <div class="form-floating">
-                                            <input type="email" class="form-control" id="email" placeholder="Your Email"/>
+                                            <input type="email" class="form-control" id="email" placeholder="Your Email" value={email}
+                                                name="email"
+                                                onChange={handleInputChange} />
                                             <label for="email">Your Email</label>
                                         </div>
                                     </div>
                                     <div class="col-12">
                                         <div class="form-floating">
-                                            <input type="text" class="form-control" id="subject" placeholder="Subject"/>
-                                            <label for="subject">Subject</label>
-                                        </div>
-                                    </div>
-                                    <div class="col-12">
-                                        <div class="form-floating">
-                                            <textarea class="form-control" placeholder="Leave a message here" id="message" style={{height: "150px"}}></textarea>
+                                            <textarea class="form-control" placeholder="Leave a message here" id="message" style={{ height: "150px" }} value={message} name={message} onChange={handleInputChange}></textarea>
                                             <label for="message">Message</label>
                                         </div>
                                     </div>
@@ -41,11 +91,17 @@ function Contact(){
                                     </div>
                                 </div>
                             </form>
+                            {errorMessage && (
+                                <div>
+                                    <p className="error-text">{errorMessage}</p>
+                                </div>
+                            )}
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+
     </>
 }
 
